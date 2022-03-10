c4d694c1effad91535480f6a217a20121584a612
JakaC-InIn <jaka.cijan@gmail.com>
2022-03-10 11:04:57 +0000

    updated entrypoint and env-sample for v2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /app && \
-    # echo "python3 NetApp.py -n $NET_APP_NAME -t $NET_API_PROT -s $NET_API_HOST -p $NET_API_PORT -U $NET_API_USER -P $NET_API_PASS -c $CALLBACK_HOST -m $CALLBACK_PORT -q $COLLECTOR_HOST -o $COLLECTOR_USER -r $COLLECTOR_PASS" &&\ 
+    # echo "python3 NetApp.py -n $NET_APP_NAME -t $NET_API_PROT -s $NET_API_HOST -p $NET_API_PORT -U $NET_API_USER -P $NET_API_PASS -c $CALLBACK_HOST -m $CALLBACK_PORT -q $COLLECTOR_HOST -o $COLLECTOR_USER -r $COLLECTOR_PASS" -z $MN_HOST -Z $MN_TOKEN &&\ 
     python3 NetApp.py \
    -n $NET_APP_NAME \
    -t $NET_API_PROT \
@@ -13,5 +13,7 @@ cd /app && \
    -m $CALLBACK_PORT \
    -q $COLLECTOR_HOST \
    -o $COLLECTOR_USER \
-   -r $COLLECTOR_PASS
+   -r $COLLECTOR_PASS \
+   -z $MN_HOST \
+   -Z $MN_TOKEN
 
